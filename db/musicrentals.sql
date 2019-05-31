428aa2eb80089a8a6eae1a3db658a74648d0525f
roderickjonathon <roderickjgillies@gmail.com>
2019-05-31 11:26:29 +0100

    SQL files created

---
+++ db/musicrentals.sql
@@ -0,0 +1,30 @@
+DROP TABLE IF EXISTS rentals;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS stocklist;
+
+CREATE TABLE customers
+( id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  phone VARCHAR(255),
+  address VARCHAR(255)
+);
+
+CREATE TABLE stocklist
+(
+  id SERIAL8 PRIMARY KEY,
+  item_name VARCHAR(255) not null,
+  item_category VARCHAR(255) not null,
+  price INT4,
+  quantity INT4
+
+);
+
+CREATE TABLE rentals
+(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  price INT4,
+  rental_date VARCHAR(255),
+  customer_id INT4 references customers(id),
+  stock_id INT4 references stocklist(id)
+)
---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+require_relative( "../models/customer.rb" )
+require_relative( "../models/rental.rb" )
+require_relative( "../models/stock.rb" )
+require('pry')
