76253735b1dee2aec8cf13832664260019b6fa3b
roderickjonathon <roderickjgillies@gmail.com>
2019-06-01 11:43:33 +0100

    Saturday morning commit

--- controllers/customer_controller.rb
+++ controllers/customer_controller.rb
@@ -7,6 +7,23 @@ require_relative('../models/rental.rb')
 also_reload('../models/*')
 
 get '/customers' do
-  @customers = Customer.new
+  @customers = Customer.all_customers
   erb( :"customers/index" )
+end
+
+get '/customers/new' do
+  @customers = Customer.all_customers
+  erb( :"customers/new" )
+end
+
+post '/customers/' do
+  customer = Customer.new(params)
+  customer.save_customer
+  redirect to ("/customers")
+end
+
+
+post '/customers/:id/delete' do
+  Customer.delete_customer(params[:id])
+  redirect to ("/customers")
 end
