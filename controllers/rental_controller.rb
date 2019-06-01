a04bb79e9f17298ec2b9811b7093471a18c7e845
roderickjonathon <roderickjgillies@gmail.com>
2019-06-01 16:27:08 +0100

    all views working

--- controllers/rental_controller.rb
+++ controllers/rental_controller.rb
@@ -10,14 +10,17 @@ also_reload('../models/*')
 get '/rentals' do
   @rental = Rental.new(params)
   @rentals = Rental.all_rentals()
-  @customer = Customer.all_customers()
+  @customers = Customer.all_customers()
   @stock = Stock.all_stock()
   erb(:"rentals/index")
 end
 
 get '/rentals/new' do
+  @rental = Rental.new(params)
+  @stockitem = Stock.new(params)
   @rentals = Rental.all_rentals
   @customer = Customer.all_customers()
+  @customers = Customer.new(params)
   @stock = Stock.all_stock()
 
   erb( :"rentals/new" )
@@ -29,11 +32,14 @@ get "/rentals/:id/" do
 end
 
 get "/rentals/:id/update" do
+  @stock = Stock.all_stock()
+  @customer = Customer.all_customers
   @rental = Rental.find(params[:id])
-  erb(:"rentals/customer_update")
+  erb(:"rentals/rental_update")
 end
 
 post "/rentals/:id/update" do
+  @customer = Customer.all
   @rental = Rental.new(params)
   @rental.update_rental()
   redirect to ("/rentals")
